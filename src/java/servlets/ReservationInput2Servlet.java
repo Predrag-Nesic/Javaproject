73628f6233f9d3bb829072454a6fc0cd02a54686
Predrag Nesic <54184774+Predrag-Nesic@users.noreply.github.com>
2021-03-13 15:31:13 +0100

    Update ReservationInput2Servlet.java

--- src/java/servlets/ReservationInput2Servlet.java
+++ src/java/servlets/ReservationInput2Servlet.java
@@ -23,46 +23,58 @@ public class ReservationInput2Servlet extends HttpServlet {
         String address = "ticket_agent.jsp";
 
 // HOW TO ADD RESERVATION AS AN EMPLOYEE
-        ArrayList<Model> users = User.all(User.class);
-        for (Model user : users) {
-            User u = (User) user;
-            if (request.getParameter("username").equals(u.getUsername())) {
-                ArrayList<Model> events = Event.all(Event.class);
-                for (Model event : events) {
-                    Event e = (Event) event;
-                    if (request.getParameter("event_name").equals(e.getName())) {
-                        reservation.setUsername(request.getParameter("username"));
-                        reservation.setEvent_name(request.getParameter("event_name"));
-                        reservation.setGfloor_tickets(Long.parseLong(request.getParameter("gfloor_tickets")));
-                        long res_gfloor_tickets = reservation.getGfloor_tickets(); //new
-                        reservation.setBalcony_tickets(Long.parseLong(request.getParameter("balcony_tickets")));
-                        long res_balcony_tickets = reservation.getBalcony_tickets(); //new
+        try {
+            ArrayList<Model> users = User.all(User.class);
+            for (Model user : users) {
+                User u = (User) user;
+                if (request.getParameter("username").equals(u.getUsername())) {
+                    ArrayList<Model> events = Event.all(Event.class);
+                    for (Model event : events) {
+                        Event e = (Event) event;
+                        if (request.getParameter("event_name").equals(e.getName())) {
+                            reservation.setUsername(request.getParameter("username"));
+                            reservation.setEvent_name(request.getParameter("event_name"));
+                            reservation.setGfloor_tickets(Long.parseLong(request.getParameter("gfloor_tickets")));
+                            long res_gfloor_tickets = reservation.getGfloor_tickets(); //new
+                            reservation.setBalcony_tickets(Long.parseLong(request.getParameter("balcony_tickets")));
+                            long res_balcony_tickets = reservation.getBalcony_tickets(); //new
 
-                        long e_gfloor_tickets = e.getGfloor_tickets() - res_gfloor_tickets;
-                        long e_balcony_tickets = e.getBalcony_tickets() - res_balcony_tickets;
-                        e.setGfloor_tickets(e_gfloor_tickets);
-                        e.setBalcony_tickets(e_balcony_tickets);
-                        e = (Event) e.save();
+                            if (res_gfloor_tickets < 0 || res_balcony_tickets < 0) {
+                                String message = "The number of tickets must be a positive integer.";
+                                request.setAttribute("message", message);
+                                address = "error.jsp";
+                                RequestDispatcher rd = request.getRequestDispatcher(address);
+                                rd.forward(request, response);
+                            }
+                            
+                            long e_gfloor_tickets = e.getGfloor_tickets() - res_gfloor_tickets;
+                            long e_balcony_tickets = e.getBalcony_tickets() - res_balcony_tickets;
+                            e.setGfloor_tickets(e_gfloor_tickets);
+                            e.setBalcony_tickets(e_balcony_tickets);
+                            e = (Event) e.save();
 
-                        reservation = (Reservation) reservation.save();
+                            reservation = (Reservation) reservation.save();
 //                        ArrayList<Model> reservations = Reservation.all(Reservation.class);//new
 //                        session.setAttribute("reservations", reservations);//new
 //                        response.sendRedirect(address);
-                        RequestDispatcher rd = request.getRequestDispatcher(address);
-                        rd.forward(request, response);
+                            RequestDispatcher rd = request.getRequestDispatcher(address);
+                            rd.forward(request, response);
+                        }
                     }
                 }
             }
+            String message = "Username or event name is not correct.";
+            request.setAttribute("message", message);
+            address = "error.jsp";
+            RequestDispatcher rd = request.getRequestDispatcher(address);
+            rd.forward(request, response);
+        } catch (NumberFormatException ex) {
+            String message = "The number of tickets must be an integer.";
+            request.setAttribute("message", message);
+            address = "error.jsp";
+            RequestDispatcher rd = request.getRequestDispatcher(address);
+            rd.forward(request, response);
         }
-        String message = "Username or event name is not correct.";
-                request.setAttribute("message", message);
-                address = "error.jsp";
-//                String back = "ticket_agent_jsp";
-//                request.setAttribute("back", back);
-
-//                response.sendRedirect(address);
-                RequestDispatcher rd = request.getRequestDispatcher(address);
-                rd.forward(request, response);
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
