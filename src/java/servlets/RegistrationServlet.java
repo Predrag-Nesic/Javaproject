da08c6a308207ef082d1dcae4b4f5b9f898011a7
Predrag Nesic <54184774+Predrag-Nesic@users.noreply.github.com>
2021-04-30 18:21:28 +0200

    Update RegistrationServlet.java

--- src/java/servlets/RegistrationServlet.java
+++ src/java/servlets/RegistrationServlet.java
@@ -18,6 +18,13 @@ public class RegistrationServlet extends HttpServlet {
       
         user.setUsername(request.getParameter("username"));
         user.setPassword(request.getParameter("password"));
+        if (!user.getPassword().equals(request.getParameter("password_repeat"))) {
+            String message = "Repeated password is incorrect.";
+            request.setAttribute("message", message);
+            String address = "error.jsp";
+            RequestDispatcher rd = request.getRequestDispatcher(address);
+            rd.forward(request, response);
+        }
         user.setFirst_name(request.getParameter("first_name"));
         user.setLast_name(request.getParameter("last_name"));
         user.setAddress(request.getParameter("address"));
